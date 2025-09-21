0681c210439cd67fab2f322da54e7f0ecd233e4c
swetha-021 <sprakash@binghamton.edu>
2025-09-20 23:45:05 -0400

    after changing the path

--- assets/assets.js
+++ assets/assets.js
@@ -1,141 +1,141 @@
-import user_image from './user-image.png';
-import code_icon from './code-icon.png';
-import code_icon_dark from './code-icon-dark.png';
-import edu_icon from './edu-icon.png';
-import edu_icon_dark from './edu-icon-dark.png';
-import project_icon from './project-icon.png';
-import project_icon_dark from './project-icon-dark.png';
-import vscode from './vscode.png';
-import firebase from './firebase.png';
-import figma from './figma.png';
-import git from './git.png';
-import mongodb from './mongodb.png';
-import right_arrow_white from './right-arrow-white.png';
-import logo from './logo.png';
-import logo_dark from './logo_dark.png';
-import mail_icon from './mail_icon.png';
-import mail_icon_dark from './mail_icon_dark.png';
-import profile_img from './profile-img.png';
-import download_icon from './download-icon.png';
-import hand_icon from './hand-icon.png';
-import header_bg_color from './header-bg-color.png';
-import moon_icon from './moon_icon.png';
-import sun_icon from './sun_icon.png';
-import arrow_icon from './arrow-icon.png';
-import arrow_icon_dark from './arrow-icon-dark.png';
-import menu_black from './menu-black.png';
-import menu_white from './menu-white.png';
-import close_black from './close-black.png';
-import close_white from './close-white.png';
-import web_icon from './web-icon.png';
-import mobile_icon from './mobile-icon.png';
-import ui_icon from './ui-icon.png';
-import graphics_icon from './graphics-icon.png';
-import right_arrow from './right-arrow.png';
-import send_icon from './send-icon.png';
-import right_arrow_bold from './right-arrow-bold.png';
-import right_arrow_bold_dark from './right-arrow-bold-dark.png';
-import postman from './postman.png';
-import uplifty from './Uplifty-icon.jpeg'
-import ghi from './GHI-icon.png'
-import optum from './optum-icon.jpeg'
-import bing from './Bing-icon.jpeg'
-import linkedin from './linkedin_icon.png'
-import github from './github-icon.png'
-import swetha from './swetha1.png'
-import android from './android.png'
-import springboot from './springboot.png'
-import flask from './flask.png'
-import xampp from './xampp.png'
+    import user_image from './user-image.png';
+    import code_icon from './code-icon.png';
+    import code_icon_dark from './code-icon-dark.png';
+    import edu_icon from './edu-icon.png';
+    import edu_icon_dark from './edu-icon-dark.png';
+    import project_icon from './project-icon.png';
+    import project_icon_dark from './project-icon-dark.png';
+    import vscode from './vscode.png';
+    import firebase from './firebase.png';
+    import figma from './figma.png';
+    import git from './git.png';
+    import mongodb from './mongodb.png';
+    import right_arrow_white from './right-arrow-white.png';
+    import logo from './logo.png';
+    import logo_dark from './logo_dark.png';
+    import mail_icon from './mail_icon.png';
+    import mail_icon_dark from './mail_icon_dark.png';
+    import profile_img from './profile-img.png';
+    import download_icon from './download-icon.png';
+    import hand_icon from './hand-icon.png';
+    import header_bg_color from './header-bg-color.png';
+    import moon_icon from './moon_icon.png';
+    import sun_icon from './sun_icon.png';
+    import arrow_icon from './arrow-icon.png';
+    import arrow_icon_dark from './arrow-icon-dark.png';
+    import menu_black from './menu-black.png';
+    import menu_white from './menu-white.png';
+    import close_black from './close-black.png';
+    import close_white from './close-white.png';
+    import web_icon from './web-icon.png';
+    import mobile_icon from './mobile-icon.png';
+    import ui_icon from './ui-icon.png';
+    import graphics_icon from './graphics-icon.png';
+    import right_arrow from './right-arrow.png';
+    import send_icon from './send-icon.png';
+    import right_arrow_bold from './right-arrow-bold.png';
+    import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+    import postman from './postman.png';
+    import uplifty from './Uplifty-icon.jpeg'
+    import ghi from './GHI-icon.png'
+    import optum from './optum-icon.jpeg'
+    import bing from './Bing-icon.jpeg'
+    import linkedin from './linkedin_icon.png'
+    import github from './github-icon.png'
+    import swetha from './swetha1.png'
+    import android from './android.png'
+    import springboot from './springboot.png'
+    import flask from './flask.png'
+    import xampp from './xampp.png'
 
-export const assets = {
-    user_image,
-    code_icon,
-    code_icon_dark,
-    edu_icon,
-    edu_icon_dark,
-    project_icon,
-    project_icon_dark,
-    vscode,
-    firebase,
-    figma,
-    git,
-    postman,
-    mongodb,
-    right_arrow_white,
-    logo,
-    logo_dark,
-    mail_icon,
-    mail_icon_dark,
-    profile_img,
-    download_icon,
-    hand_icon,
-    header_bg_color,
-    moon_icon,
-    sun_icon,
-    arrow_icon,
-    arrow_icon_dark,
-    menu_black,
-    menu_white,
-    close_black,
-    close_white,
-    web_icon,
-    mobile_icon,
-    ui_icon,
-    graphics_icon,
-    right_arrow,
-    send_icon,
-    right_arrow_bold,
-    right_arrow_bold_dark,
-    uplifty,
-    ghi,
-    optum,
-    bing,
-    linkedin,
-    github,
-    swetha,
-    android,
-    flask,
-    springboot,
-    xampp,
-};
+    export const assets = {
+        user_image,
+        code_icon,
+        code_icon_dark,
+        edu_icon,
+        edu_icon_dark,
+        project_icon,
+        project_icon_dark,
+        vscode,
+        firebase,
+        figma,
+        git,
+        postman,
+        mongodb,
+        right_arrow_white,
+        logo,
+        logo_dark,
+        mail_icon,
+        mail_icon_dark,
+        profile_img,
+        download_icon,
+        hand_icon,
+        header_bg_color,
+        moon_icon,
+        sun_icon,
+        arrow_icon,
+        arrow_icon_dark,
+        menu_black,
+        menu_white,
+        close_black,
+        close_white,
+        web_icon,
+        mobile_icon,
+        ui_icon,
+        graphics_icon,
+        right_arrow,
+        send_icon,
+        right_arrow_bold,
+        right_arrow_bold_dark,
+        uplifty,
+        ghi,
+        optum,
+        bing,
+        linkedin,
+        github,
+        swetha,
+        android,
+        flask,
+        springboot,
+        xampp,
+    };
 
-export const workData = [
-    {
-        title: 'Chef Claude',
-        description: 'React app using an AI API to generate creative recipes',
-        bgImage: '/work-1.png',
-    },
-    {
-        title: 'ToDo List',
-        description: 'A task management app built with PHP and Laravel',
-        bgImage: '/work-2.png',
-    },
-    {
-        title: 'Tenzies',
-        description: 'A fun dice-rolling game made with React and JavaScript',
-        bgImage: '/work-3.png',
-    },
-    {
-        title: 'Hangman',
-        description: 'A classic Hangman word game built with JavaScript.',
-        bgImage: '/work-4.png',
-    },
-]
+    export const workData = [
+        {
+            title: 'Chef Claude',
+            description: 'React app using an AI API to generate creative recipes',
+            bgImage: './work-1.png',
+        },
+        {
+            title: 'ToDo List',
+            description: 'A task management app built with PHP and Laravel',
+            bgImage: './work-2.png',
+        },
+        {
+            title: 'Tenzies',
+            description: 'A fun dice-rolling game made with React and JavaScript',
+            bgImage: './work-3.png',
+        },
+        {
+            title: 'Hangman',
+            description: 'A classic Hangman word game built with JavaScript.',
+            bgImage: './work-4.png',
+        },
+    ]
 
-export const experienceData = [
-    { icon: assets.uplifty, title: 'Uplifty AI', position: 'Team Lead, iOS & Android App Developer, UI/UX Developer', location: 'Austin, TX ' },
-    { icon: assets.ghi, title: 'Global Health Impact', position: 'Full Stack Developer', location: 'Binghamton, NY' },
-    { icon: assets.optum, title: 'Optum', position: 'Java Backend Developer', location: 'Hyderabad, India' },
-    { icon: assets.bing, title: 'Binghamton University', position: 'Teaching Assistant', location: 'Binghamton, NY' },
-]
+    export const experienceData = [
+        { icon: assets.uplifty, title: 'Uplifty AI', position: 'Team Lead, iOS & Android App Developer, UI/UX Developer', location: 'Austin, TX ' },
+        { icon: assets.ghi, title: 'Global Health Impact', position: 'Full Stack Developer', location: 'Binghamton, NY' },
+        { icon: assets.optum, title: 'Optum', position: 'Java Backend Developer', location: 'Hyderabad, India' },
+        { icon: assets.bing, title: 'Binghamton University', position: 'Teaching Assistant', location: 'Binghamton, NY' },
+    ]
 
-export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Java, Python, C++, HTML, CSS, JavaScript React Js, Next Js, React Native' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Libraries & Frameworks', description: 'Spring Boot, Flask, React.js, Next.js, React Native' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Masters in Computer Science - Binghamton University' },
-];
+    export const infoList = [
+        { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Java, Python, C++, HTML, CSS, JavaScript React Js, Next Js, React Native' },
+        { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Libraries & Frameworks', description: 'Spring Boot, Flask, React.js, Next.js, React Native' },
+        { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Masters in Computer Science - Binghamton University' },
+    ];
 
-export const toolsData = [
-    assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git,assets.postman,assets.android,assets.springboot,assets.flask,assets.xampp,
-];
+    export const toolsData = [
+        assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git,assets.postman,assets.android,assets.springboot,assets.flask,assets.xampp,
+    ];
