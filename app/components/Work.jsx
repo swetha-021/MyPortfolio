54672222d2f8f30622d973f16ffcaf219393455d
swetha-021 <sprakash@binghamton.edu>
2026-04-09 14:20:07 -0700

    ui fix

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -37,21 +37,19 @@ const Work = () => {
                 whileHover={{scale:1.05}}
                 transition={{duration:0.2}}
                 key={index} style={{backgroundImage:`url(${project.bgImage})`}} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'>
+                    <a href={project.link} target="_blank" rel="noopener noreferrer" className="flex items-center justify-center w-full h-full">
                     <div className='bg-[#44b1d8] w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
-                        <a href={project.link} target="_blank" rel="noopener noreferrer" className="flex items-center justify-center w-full h-full">
                         <div>
                             <h2 className='font-semibold'>{project.title}</h2>
                             <p className='text-sm text-white'>{project.description}</p>
                         </div>
 
                         <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-white transition'>
-        
-                                <Image src={assets.send_icon} alt='send icon' className='w-5'/>
-                            
+                            <Image src={assets.send_icon} alt='send icon' className='w-5'/>
                         </div>
-                        </a>
                         
                     </div>
+                    </a>
                 </motion.div>
             ))}
         </motion.div>
