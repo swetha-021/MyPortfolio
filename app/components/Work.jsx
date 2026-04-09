9d811fb024b8c45ce055ad29ad11e61e8e133781
swetha-021 <sprakash@binghamton.edu>
2026-04-09 14:25:15 -0700

    updated card

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -37,8 +37,14 @@ const Work = () => {
                 whileHover={{scale:1.05}}
                 transition={{duration:0.2}}
                 key={index} style={{backgroundImage:`url(${project.bgImage})`}} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'>
-                    <a href={project.link} target="_blank" rel="noopener noreferrer" className="flex items-center justify-center w-full h-full">
-                    <div className='bg-[#44b1d8] w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
+                    {/* <a href={project.link} target="_blank" rel="noopener noreferrer" className="flex items-center justify-center w-full h-full"> */}
+                    {/* <div className='bg-[#44b1d8] w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'> */}
+                    <a
+                        href={project.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='bg-[#44b1d8] w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'
+                    >
                         <div>
                             <h2 className='font-semibold'>{project.title}</h2>
                             <p className='text-sm text-white'>{project.description}</p>
@@ -48,7 +54,7 @@ const Work = () => {
                             <Image src={assets.send_icon} alt='send icon' className='w-5'/>
                         </div>
                         
-                    </div>
+                    {/* </div> */}
                     </a>
                 </motion.div>
             ))}
